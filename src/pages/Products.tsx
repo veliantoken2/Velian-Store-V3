d9a170bc578988aa6b582bfee71e8ddc82cbfb08
veliantoken2 <rikinakinn@gmail.com>
2025-09-06 18:50:19 +0700

    Updated index.css

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -369,9 +369,9 @@ const Products = () => {
             </div>
 
             {/* Products Grid */}
-            <div className={`grid gap-3 sm:gap-6 ${
+            <div className={`grid gap-4 sm:gap-6 ${
               viewMode === 'grid' 
-                ? 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-3' 
+                ? 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3' 
                 : 'grid-cols-1'
             }`}>
               {sortedProducts.map(product => (
